30874be0bf7b7cb577df2de88b084abd5ba1e9dd
Farzana Khanom <139230813+farzana-023@users.noreply.github.com>
2023-10-03 17:31:13 +0600

    Create Bear and Big Brother.cpp

---
+++ lab-7/Bear and Big Brother.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+                int a,b, y=1;
+                cin>>a>>b;
+
+                while(1)
+                {
+                       a = a*3;
+                                b= b*2;
+                                if (a>b)
+                                {
+                                                cout<<y<<endl;
+                                                break;
+                                }
+                                y++;
+                }
+                return 0;
+
+                }
+
